a0e3af025ab951c25a3264b4c000d202006a48a4
Akshay-agarwal <akshay.agarwal01@sjsu.edu>
2017-10-11 12:48:57 -0700

    PostOrder

---
+++ Problems/Tree/145.PostOrder.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        stack1, stack2, result = [root], [], []
+        while stack1:
+            node = stack1.pop()
+            stack2.append(node)
+            if node.left:
+                stack1.append(node.left)
+            if node.right:
+                stack1.append(node.right)
+
+        while stack2:
+            result.append(stack2.pop().val)
+
+        return result
